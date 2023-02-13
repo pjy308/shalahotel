52b4ae8a87d88bfd49aa87f1830997e351e02324
박재윤 <bykimty17@naver.com>
2023-02-13 11:27:13 +0900

    first fstweb

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function viewTabsOn(){
+    let v = document.getElementById("mytabs");
+    if(v.style.display == 'none'){
+        v.style.display = 'block';
+    }
+    else v.style.display = 'none';
+}
